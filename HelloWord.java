3c7f4eab7069b719d0d341f9e5fd51306ac82945
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-09 19:58:54 +0700

    Hari 1

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+package pkg100.days;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("hello java");
+        System.out.println("Nama : Nasira");
+        System.out.println ("Kelas : Informatika A 23") ;
+        System.out.println ("NIM : D0223304");
+        System.out.println("Alamat : Enrekang");
+    }
+}
