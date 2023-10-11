cd23563c01ec0db3c0c303d0196883f237886b77
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-11 19:55:39 +0700

    Hari 3

---
+++ Hariketiga.java
@@ -0,0 +1,12 @@
+package pkg100Days;
+
+ class Hariketiga{
+    
+    public static void main(String[] args) {
+    String  angka1 = "13";
+     int angka2 = 10;
+        System.out.println("angka1"+ 10);
+
+    
+    }
+}
