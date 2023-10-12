4c17212bae7941ee2e2deec0fa4f0d9b49c933b0
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-13 00:29:36 +0700

    Hari 4

---
+++ Harikeempat.java
@@ -0,0 +1,12 @@
+package pkg100Days;
+
+ class Harikeempat {
+    
+    public static void main(String[] args) {
+      int angka = 3;
+        angka*= 5;
+        System.out.println(angka);
+        
+    }
+    
+}
