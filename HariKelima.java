304bd7cdbe3fb05560fdefb6ede7d0693d37690c
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-13 00:32:53 +0700

    Hari 5

---
+++ HariKelima.java
@@ -0,0 +1,13 @@
+package pkg100Days;
+
+ class Harikelima {
+    
+    public static void main(String[] args) {
+      int nilai = 5;
+        int angka = 13;
+        System.out.println(angka<nilai);
+        
+        
+    }
+    
+}
