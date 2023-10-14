a4927e84d17fb931a6357a7b6a955d3dc115b9a1
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-14 08:04:47 +0700

    Hari 6

---
+++ Harikeenam.java
@@ -0,0 +1,17 @@
+package pkg100Days;
+
+ class Harikeenam{
+    
+    public static void main(String[] args) {
+
+    
+    char nilai1 = '7';
+    char nilai2 = '9';
+    System.out.print('7' + 9);
+
+    
+}
+
+        
+        
+    }
