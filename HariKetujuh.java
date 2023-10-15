c6abe16a383a24af9c537476eb8cff0fa7e2b81d
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-15 07:36:10 +0700

    Hari 7

---
+++ HariKetujuh.java
@@ -0,0 +1,17 @@
+package pkg100Days;
+
+ class Hariketujuh{
+    
+    public static void main(String[] args) {
+
+    
+    byte umur = '9';
+    umur = '9';
+    System.out.print('9' + umur);
+
+    
+}
+
+        
+        
+    }
