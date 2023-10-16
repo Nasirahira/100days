9e94f6929d6e38943c56a111b7d83d01ad9a0f0c
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-16 18:01:12 +0700

    Hari 8

---
+++ Harikedelapan.java
@@ -0,0 +1,21 @@
+package pkg100Days;
+
+class Harikedelapan {
+    
+    public static void main(String[] args) {
+        byte umur = 19;
+        umur = 19;
+        String nama = "Nasira";
+        String nama_tengah = "berumur";
+        String nama_belakang = "tahun";
+        System.out.println(nama +" " +nama_tengah + umur +" " +nama_belakang);
+    }
+    
+}
+
+      
+      
+        
+      
+    
+    
