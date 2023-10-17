def190015439d4ceaf545d85d6d9c72fd9b9f4d4
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-17 18:13:35 +0700

    Hari 9

---
+++ Harikesembilan.java
@@ -0,0 +1,22 @@
+package pkg100days;
+ class Harikesembilan{
+    
+    public static void main(String[] args) {
+        byte umur = 19;
+        umur = 19;
+        float berat = 52.4f;
+        berat = 52.4f;
+        double tinggi=152.4;
+        tinggi =152.4;
+        String nama_kanan ="serta tinggi badan";
+        String nama_kiri=" berat badan";
+        String nama_samping ="dan memiliki";
+        
+        String nama = "Nasira";
+        String nama_tengah = "berumur";
+        String nama_belakang = "tahun";
+        
+        System.out.println(" nama " + " " +nama_tengah + umur +" " +nama_belakang +" " +nama_samping + " "+ nama_kiri + " " + berat + " " + nama_kanan + " " + tinggi);
+    }
+    
+}
