2953a45ef1d30c09dd59413def583263bd5f629b
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-18 19:18:24 +0700

    Hari 10

---
+++ Harikesepuluh.java
@@ -0,0 +1,18 @@
+package pkg100days;
+
+class Harikesepuluh{
+    
+    public static void main(String[] args) {
+        int angka = 7;
+        
+        if(angka > 7){
+            System.out.println("salah");
+            
+        }else if(angka > 9){
+            System.out.println("benar");
+        }else{
+            System.out.println("santui");
+        }
+    }
+    
+}
