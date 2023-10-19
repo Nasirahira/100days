175010d71fcb767289f155cd1d2fe15eeb2ab8d8
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-19 19:28:41 +0700

    Hari 11

---
+++ Harikesebelas.java
@@ -0,0 +1,22 @@
+package pkg100days;
+
+ class Harikesebelas {
+    
+    public static void main(String[] args){
+        
+        int angka = 7;
+        int bilangan = 7;
+        
+        
+        if(angka < bilangan){
+            System.out.println("salah");
+            
+        }else if(7 == 7){
+            System.out.println("benar");
+        }else{
+            System.out.println("angka == bilangan ");
+        }
+    }
+    
+
+    }
