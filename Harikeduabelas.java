8335991bb2d7195b9202509f3e3fb676ba8cdb52
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-20 17:50:46 +0700

    Hari 12

---
+++ Harikeduabelas.java
@@ -0,0 +1,15 @@
+package pkg100days;
+
+ class Harikeduabelas {
+    
+    public static void main(String[] args) {
+        
+
+        float angka1 =53.3f;
+       float angka2 = 15.5f;
+        
+        System.out.println(53.3f  * 15.5f);
+        
+    }
+    
+}
