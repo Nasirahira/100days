a8b3eff61d579e9cd90998d98376c8f923182f75
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-22 21:02:43 +0700

    Hari 14

---
+++ Harikeempatbelas.java
@@ -0,0 +1,27 @@
+package pkg100days;
+
+ class Harikeempatbelas {
+    
+    public static void main(String[] args) {
+  cek_zodiak(tanggal,bulan);
+    if (bulan == 3 && tanggal >= 21 || bulan == 4 && tanggal <= 19);
+        return "Aries";
+    elif (bulan == 4 && tanggal >= 20 || bulan == 5 && tanggal <= 20);
+        return "Taurus";
+    elif (bulan == 5 && tanggal >= 21 ||  bulan == 6 && tanggal <= 20);
+        return "Gemini";
+    elif (bulan == 6 && tanggal >= 21 || bulan == 7 && tanggal <= 22);
+        return "Cancer";
+    elif (bulan == 7 && tanggal >= 23 || bulan == 8 && tanggal <= 22);
+        return "Leo";
+    elif (bulan == 8 && tanggal >= 23 || bulan == 9 && tanggal <= 22);
+        return "Virgo";
+    elif (bulan == 9 && tanggal >= 23 || bulan == 10 && tanggal <= 22);
+        return "Libra";
+        
+    else:
+        return" Pisces";
+        System.out.println(bulan == 3 && tanggal >= 23 || tanggal == 20 && bulan <= 2);
+    }   
+        
+    }
