a2f2dddd7aaf9669ec700d349edf60e4346c20aa
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-25 18:55:52 +0700

    Hari 17

---
+++ Hariketujuhbelas.java
@@ -0,0 +1,24 @@
+package pkg100daya;
+
+class Hariketujuhbelas{
+    
+    public static void main(String[] args) {
+     
+     
+    hobi = "volly ball" + "olahraga";
+
+        print(olahraga)
+
+
+       hobi.add("volly ball");
+
+
+       print(vollyball);
+
+
+      hobi.remove("olahraga");
+
+
+       print(hobi);
+    }     
+}
