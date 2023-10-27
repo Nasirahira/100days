85ce395d9c97061424dc8ebb3f600afcc6efe862
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-28 00:27:00 +0700

    Hari 20

---
+++ Harikeduapuluh.java
@@ -0,0 +1,23 @@
+package pkg100days;
+
+ class Harikeduapuluh {
+    
+    public static void main(String[] args) {
+        
+          nama = "Kasmia"; 
+          umur = 31;
+          print("Halo, nama saya " + Kasmia);
+
+          print("Saya berusia " + str(31) + " tahun");
+        if (umur >=17){
+            
+        
+           print("Saya sudah dewasa");
+        } else  (umur <= 18){
+            
+             System.out.println("Saya masih dibawah umur");
+            
+        }    
+    }
+    
+}
