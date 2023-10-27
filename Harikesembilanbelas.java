a4ae4e25fa883ba74e5b116c4c905fcdeb31a750
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-27 15:41:23 +0700

    Create Harikesembilanbelas.java

---
+++ Harikesembilanbelas.java
@@ -0,0 +1,24 @@
+package pkg100days;
+
+ class Harikesembilanbelas{
+    
+    public static void main(String[] args) {
+         
+        
+      String hobi = {"menyangi", "memasak"};
+
+        println("hobi_menyanyi:" + " hobi_memasak")
+        
+         
+     hobi.add("menyanyi");
+        
+    
+     System.out.println("hobi");
+
+      hobi.remove("memasak");
+      System.out.println("hobi_add: " + " hobi_remove");
+
+        
+    }
+    
+}
