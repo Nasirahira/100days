ab8c515a66e1c3bb575e8731f14fb8aa65067462
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-29 05:23:32 +0700

    Hari 21

---
+++ Harikeduaputuhsatu.java
@@ -0,0 +1,10 @@
+package pkg100days;
+class Harikeduapuluhsatu {
+  public static void main(Satring[] args) {
+    hitung_rata_rata(daftar_angka);
+    rata_rata = total / len(angka);
+    angka = ("6,12,18,24,30");
+  rata_rata = hitung_rata_rata (daftar_angka);
+    print("rata_rata dari angka-angka ini adalah:",rata_rata);
+  }
+}
