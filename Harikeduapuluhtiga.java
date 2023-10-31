d51b75be9324b3cf709864bbb2425165ed517bb3
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-10-31 10:02:31 +0700

    Hari 23

---
+++ Harikeduapuluhtiga.java
@@ -0,0 +1,10 @@
+package pkg100days;
+
+class Harikeduapuluhtiga{
+ public static void main(String[] args ) {
+
+total = 0;
+for i in range(1, 101);
+    if( i % 2 ) != "0";
+        total += i;
+print("Jumlah semua angka ganjil dari 1 hingga 100 adalah: {total},");
