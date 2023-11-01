6fef78cabad8756ff482275bd3a4c3f1b2578289
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-01 18:39:24 +0700

    Hari 24

---
+++ Harikeduapuluhempat.java
@@ -0,0 +1,14 @@
+package pkh100daus;
+class Harikeduapuluhempat.java{
+  public static void main(String [] args {
+    
+x = 5;
+y = 10;
+
+
+    print(f"Koordinat x: {x}");
+    print(f"Koordinat y: {y}");
+  }
+}
+    
+    
