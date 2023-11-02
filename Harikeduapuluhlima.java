7015fab6f49018a7add09eb4a93b3e8a7b3c6b48
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-02 19:58:36 +0700

    Hari 25

---
+++ Harikeduapuluhlima.java
@@ -0,0 +1,14 @@
+package pkg100days;
+class Harikedapuluhlima {
+
+  public static voic main(String [] args ){
+     resistor1 = 10;
+     resistor2 = 20;
+     resistor3 = 30;
+
+tegangan_sumber = 50;
+tegangan_total = resistor1 + resistor2 + resistor3;
+
+print("Tegangan Total dalam Rangkaian Seri:", tegangan_total, "Volt");
+  }
+}
