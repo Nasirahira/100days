22819ae1a1f3374fe4c189c3e7a3b241bf6f167b
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-04 05:40:37 +0700

    Hari 27

---
+++ Harikeduapuluhtujuh.java
@@ -0,0 +1,16 @@
+package pkg100days;
+class Harikeduapuluhtujuh {
+oublic static void main(String[] args) {
+
+
+def factorial(n);
+    if n == 0;
+        return 1;
+} else{
+        return n * factorial(n - 1);
+
+number = 5;
+result = factorial(number);
+print(f"The factorial of {number} is {result}");
+}
+}
