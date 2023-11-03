a049274da51ea0276cbf313889e7f6a65bb94b5e
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-03 15:17:56 +0700

    Hari 26

---
+++ Harikeduapuluhenam.java
@@ -0,0 +1,13 @@
+package pkg100days;
+
+ class Harikeduapuluhenam {
+    
+    public static void main(String[] args) {
+        String resistor1 = "10";
+        String resistor2 = "30";
+        String resistor3 = "50";
+         System.out.println("resistor1 * resistor2 - resistor3");
+        
+        
+    }
+    }
