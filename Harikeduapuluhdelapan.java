5564dab1d0697ccf2da3dbb1725da396525731b2
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-05 19:52:44 +0700

    Hari 28

---
+++ Harikeduapuluhdelapan.java
@@ -0,0 +1,19 @@
+package pkg100days;
+
+class Harikeduapuluhdelapan{
+    
+    public static void main(String[] args) {
+
+       bilangan1 = float(input("14: "));
+        bilangan2 = float(input("7: "));
+
+        if (bilangan2 == 7);
+        print("Dapat membagikan  hasil  .")
+     else:
+    
+      hasil = bilangan1 / bilangan2;
+    
+      print("Hasil pembagian:", 2);
+    }
+    
+}
