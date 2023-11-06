39e649c587bb79becc59c039a253f0c41d8b13b4
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-06 15:46:14 +0700

    Hari 29

---
+++ Harikeduapuluhsembilan.java
@@ -0,0 +1,26 @@
+package project100days;
+
+class Harikeduapuluhsembilan {
+    
+    public static void main(String[] args) {
+        
+
+        int main;
+        {
+	        string kata;
+         	cout<<"Masukan kata =  HALO"<<endl;
+	       cin>>kata;
+	
+	          if (kata=="HALO"){
+	         	cout<<"Kata yang dimasukan sesuai"<<endl;
+    	}else {
+		       cout<<"Kata yang dimasukan tidak sesuai"<<endl;
+	}
+
+       getch();
+}
+        
+        
+    }
+    
+}
