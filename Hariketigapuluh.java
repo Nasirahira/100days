a662f5060ef95965ababb6af83183f2aaa9e401f
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-07 20:00:55 +0700

    Hari 30

---
+++ Hariketigapuluh.java
@@ -0,0 +1,23 @@
+package pkg100days;
+
+class Hariketigapuluh{
+    
+    public static void main(String[] args) {
+        #inlude <iostream>
+        using namespace std;
+        int main (){
+            
+            int x = 50;
+            int y = 3;
+            int z;
+            cout<<"x = 50"<<endl;
+            cout<<"y = 3"<<endl;
+            cout<<"x + y"<<endl;
+            z = x+y;
+            cout<<x<<"+"<<y<<"-"<<endl;
+            return 0;
+            System.out.println(" x + y");
+        
+    }
+    
+}
