7627938be48f09b8ab103dbefedea33082b7ac5b
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-08 18:58:43 +0700

    Hari 31

---
+++ Hariketigapuluhsatu.java
@@ -0,0 +1,21 @@
+package pkgNasira;
+import java.util.Scanner;
+
+ class Hariketigapuluhsatu{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah bilangan: ");
+        int bilangan = input.nextInt();
+
+        if (bilangan > 0) {
+            System.out.println("Bilangan ini adalah positif.");
+        } else if (bilangan < 0) {
+            System.out.println("Bilangan ini adalah negatif.");
+        } else {
+            System.out.println("Bilangan ini adalah nol.");
+        }
+
+        input.close();
+    }
+}
