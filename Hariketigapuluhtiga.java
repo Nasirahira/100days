e034844a4469fbcc729772b4f62b4c1717d4d326
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-10 20:43:23 +0700

    Hari 33

---
+++ Hariketigapuluhtiga.java
@@ -0,0 +1,23 @@
+package pkg100days;
+
+import java.util.Scanner;
+
+ class Hariketigapuluhtiga {
+    
+    public static void main(String[] args){
+    def = cari_nilai_maksimum(arr);
+    if (elemen > maksimum = 0);
+        return "Array kosong";
+    
+    maksimum = arr[0];
+        for (elemen in: arr);
+        if (elemen > maksimum);
+            maksimum = elemen;
+    
+    return maksimum;
+        
+       input_array = ("5, 12, 7, 15, 3, 9");
+       hasil = cari_nilai_maksimum(input_array);
+       print("Nilai maksimum dalam array adalah: {hasil}");
+    }
+}
