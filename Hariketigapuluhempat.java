22938373d5815a1786c4489c8576872c7d0985ec
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-11 18:46:44 +0700

    Hari 34

---
+++ Hariketigapuluhempat.java
@@ -0,0 +1,26 @@
+package Nasira;
+
+import java.util.Scanner;
+
+class Hariketigapuluhempat{
+    
+    public static void main(String[] args){
+        
+
+    print("Selamat datang di Dunia Petualangan!");
+    time.sleep(1);
+    pilihan = input("Anda berada di depan pintu misterius. Mau membuka pintu? (ya/tidak): ")
+
+    if (pilihan.lower()) == "ya";
+    print("Anda membuka pintu dan menemukan ruangan gelap.");
+    time.sleep(1);
+    pilihan_gelap = input("Hidupkan lampu? (ya/tidak): ")
+
+      if pilihan_gelap.lower() == "ya";
+        print("Anda menemukan kunci dan berhasil keluar!")
+    else:
+        print("Anda tersandung dan jatuh ke dalam lubang. Petualangan berakhir.")
+     else:
+         print("Anda memilih untuk pergi. Petualangan berakhir.");
+    }
+}
