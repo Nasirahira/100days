5017dbf117f94318126ccfdfbdb83cbea66781b6
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-13 20:01:22 +0700

    Hari 36

---
+++ Hariketigapuluhenam.java
@@ -0,0 +1,23 @@
+package pkg100adays;
+
+import java.util.Scanner;
+
+class Hariketigapuluhenam{
+    
+    public static void main(String[] args ){
+        
+            int hitungKuadrat(int x){
+            return x * x;
+          }
+             int main() {
+           int angka = 5;
+             int hasil = hitungKuadrat(angka);
+             }
+          std::cout << "Kuadrat dari " << angka << " adalah " << hasil << std::endl;
+    
+          return 0;
+            
+    }         
+        
+        
+        }
