a3b02959f19396bf10e39f12f0000d7f0e25136d
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-14 20:45:57 +0700

    Hari 37

---
+++ Hariketigapuluhtujuh.java
@@ -0,0 +1,26 @@
+package Nasira;
+
+import java.util.Scanner;
+
+class Hariketigapuluhtujuh {
+   
+    public static void main(String[] args) {
+     include<iostream>
+        #include<String>
+    int main[]{
+    
+    cout<<"String nama<<";
+    int usia;
+
+    
+    cout<<"Nasira : "<<endl;
+    cout<<"19tahun : "<<endl;
+    cin>>usia;
+    cout<<"Halo, " << nama << "!"<<endl;
+    cout<<"Anda berusia " << usia << " tahun."<<endl;
+
+    return 0;
+    }
+
+    }
+}
