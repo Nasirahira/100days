95a09c6e5791e2836f5600af1a73ce986bcc7503
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-15 20:54:16 +0700

    Hari 38

---
+++ Hariketigapuluhdelapan.java
@@ -0,0 +1,18 @@
+package Nasira;
+
+import java.util.Scanner;
+
+class Hariketigapuluhdelapan {
+    
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+       char nilaiMutu = input.next().charAt(0);
+        char nilai = 7;
+       System.out.println("nilai anda adalah = "+7);
+       
+        String nama = input.nextLine();
+          String nama1 = "Nasira";
+        System.out.println("nama yang anda ketik adalah : "+ nama);
+    }
+}
