62f549484997ccf246a182cc5243321a6e450c29
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-16 19:34:30 +0700

    Hari 39

---
+++ Hariketigapuluhsembilan.java
@@ -0,0 +1,17 @@
+package pkg100days;
+
+import java.util.Scanner;
+
+ class Hariketigapuluhsembilan {
+    public static void main(String[] args){
+        
+        hitung_keliling_lingkaran(jari_jari):
+    keliling = 2 * math.pi * jari_jari;
+    return keliling;
+       jari_jari = f(input("Masukkan jari-jari lingkaran: "));
+
+        keliling = hitung_keliling_lingkaran(jari_jari);
+
+        System.out.println("Keliling lingkaran dengan jari-jari {jari_jari} adalah: {keliling}");
+    }
+}
