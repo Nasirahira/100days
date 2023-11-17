018134b33c8e4434e0f45e8cb5d6b274266ebd98
Nasirahira <147214797+Nasirahira@users.noreply.github.com>
2023-11-17 20:15:47 +0700

    Hari 40

---
+++ Harikeempatpuluh.java
@@ -0,0 +1,17 @@
+package pkg100days;
+
+public class OperatorPembanding {
+    
+  public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 10;
+
+    
+        System.out.println("Apakah angka1 sama dengan angka2? " + (angka1 == angka2));
+        System.out.println("Apakah angka1 tidak sama dengan angka2? " + (angka1 != angka2));
+        System.out.println("Apakah angka1 lebih besar dari angka2? " + (angka1 > angka2));
+        System.out.println("Apakah angka1 lebih kecil dari angka2? " + (angka1 < angka2));
+        System.out.println("Apakah angka1 lebih besar atau sama dengan angka2? " + (angka1 >= angka2));
+        System.out.println("Apakah angka1 lebih kecil atau sama dengan angka2? " + (angka1 <= angka2));
+    }
+}
